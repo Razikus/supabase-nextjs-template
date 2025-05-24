fb311b5f5acee1d81b02b84d75296d1473020cb4
Adrian Rinnus <adrian.rinnus@gmail.com>
2025-05-24 23:56:54 +0200

    Add tanstack query provider.

--- nextjs/src/app/app/layout.tsx
+++ nextjs/src/app/app/layout.tsx
@@ -1,11 +1,14 @@
-// src/app/app/layout.tsx
+'use client';
 import AppLayout from '@/components/AppLayout';
-import { GlobalProvider } from '@/lib/context/GlobalContext';
+import {GlobalProvider} from '@/lib/context/GlobalContext';
+import {TanstackQueryClientProvider} from "@/providers/query-provider";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({children}: { children: React.ReactNode }) {
     return (
-        <GlobalProvider>
-            <AppLayout>{children}</AppLayout>
-        </GlobalProvider>
+        <TanstackQueryClientProvider>
+            <GlobalProvider>
+                <AppLayout>{children}</AppLayout>
+            </GlobalProvider>
+        </TanstackQueryClientProvider>
     );
 }
