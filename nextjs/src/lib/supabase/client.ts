27249408db7a6be2e2eb171bd6032d3088a72de2
LeftFollowsRight <stephen@leftfollowsright.com>
2025-08-07 14:12:52 +0100

    Update client.ts

--- nextjs/src/lib/supabase/client.ts
+++ nextjs/src/lib/supabase/client.ts
@@ -1,10 +1,10 @@
 // nextjs/src/lib/supabase/client.ts
-import { createBrowserClient } from '@supabase/ssr';
-import { Database } from '@/lib/database.types';
+import { createBrowserClient } from '@supabase/ssr'
+import { Database } from '@/lib/database.types'
 
-export function createClient() {
+export const createClient = () => {
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 }
