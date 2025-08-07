65cf45cf511d757d07934fbac45784b5f890146d
LeftFollowsRight <stephen@leftfollowsright.com>
2025-08-07 16:23:44 +0100

    Update middleware.ts

--- nextjs/src/lib/supabase/middleware.ts
+++ nextjs/src/lib/supabase/middleware.ts
@@ -1,11 +1,28 @@
-// nextjs/src/lib/supabase/middleware.ts
+// nextjs/src/middleware.ts
 import { createMiddlewareClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import { Database } from '@/lib/database.types'
 
-export async function updateSession(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const response = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ request, response })
+  const supabase = createMiddlewareClient({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    request,
+    response,
+  })
   await supabase.auth.getSession()
   return response
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico).*)',
+  ],
 }
