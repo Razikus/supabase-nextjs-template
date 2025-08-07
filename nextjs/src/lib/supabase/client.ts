b1f962802dce8ea45c32d70a72a45bdcc3929ea6
LeftFollowsRight <stephen@leftfollowsright.com>
2025-08-07 14:01:40 +0100

    Update client.ts

--- nextjs/src/lib/supabase/client.ts
+++ nextjs/src/lib/supabase/client.ts
@@ -1,24 +1,10 @@
-import {createBrowserClient} from '@supabase/ssr'
-import {ClientType, SassClient} from "@/lib/supabase/unified";
-import {Database} from "@/lib/types";
+// nextjs/src/lib/supabase/client.ts
+import { createBrowserClient } from '@supabase/ssr';
+import { Database } from '@/lib/database.types';
 
-export function createSPAClient() {
-    return createBrowserClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
-}
-
-export async function createSPASassClient() {
-    const client = createSPAClient();
-    return new SassClient(client, ClientType.SPA);
-}
-
-export async function createSPASassClientAuthenticated() {
-    const client = createSPAClient();
-    const user = await client.auth.getSession();
-    if (!user.data || !user.data.session) {
-        window.location.href = '/auth/login';
-    }
-    return new SassClient(client, ClientType.SPA);
+export function createClient() {
+  return createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
 }
