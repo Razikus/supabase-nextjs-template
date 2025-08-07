3dd20668b8fbcc4875fc7f4c6e7e8a51d0df85cb
LeftFollowsRight <stephen@leftfollowsright.com>
2025-08-07 16:19:37 +0100

    Update server.ts

--- nextjs/src/lib/supabase/server.ts
+++ nextjs/src/lib/supabase/server.ts
@@ -1,38 +1,26 @@
-import {createServerClient} from '@supabase/ssr'
-import {cookies} from 'next/headers'
-import {ClientType, SassClient} from "@/lib/supabase/unified";
-import {Database} from "@/lib/types";
+// nextjs/src/lib/supabase/server.ts
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+import { Database } from '@/lib/database.types'
 
-export async function createSSRClient() {
-    const cookieStore = await cookies()
+export const createClient = () => {
+  const cookieStore = cookies()
 
-    return createServerClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        {
-            cookies: {
-                getAll() {
-                    return cookieStore.getAll()
-                },
-                setAll(cookiesToSet) {
-                    try {
-                        cookiesToSet.forEach(({ name, value, options }) =>
-                            cookieStore.set(name, value, options)
-                        )
-                    } catch {
-                        // The `setAll` method was called from a Server Component.
-                        // This can be ignored if you have middleware refreshing
-                        // user sessions.
-                    }
-                },
-            }
-        }
-    )
-}
-
-
-
-export async function createSSRSassClient() {
-    const client = await createSSRClient();
-    return new SassClient(client, ClientType.SERVER);
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
 }
